c3190b0eeed372f73eea307dd480585060d7c549
kwhome <kw@dancinganchovy.com>
2015-10-30 23:53:12 +0900

    초 마다 마우스 위치 찾는 기능 추가

--- ScreenCaptureToOcrToDic/ScreenCaptureToOcrToDic/Program.cs
+++ ScreenCaptureToOcrToDic/ScreenCaptureToOcrToDic/Program.cs
@@ -3,13 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace ScreenCaptureToOcrToDic
 {
+    using System.Timers;
+
     class Program
     {
+        private static Timer aTimer;
+
         static void Main(string[] args)
         {
+            // 1초의 interval을 둔 timer 만들기
+            aTimer = new Timer(1000);
+
+            // Hook up the Elapsed event for the timer.
+            aTimer.Elapsed += OnTimedEvent;
+            aTimer.Enabled = true;
+
+            Console.WriteLine("Press the Enter key to exit the program... ");
+            Console.ReadLine();
+            Console.WriteLine("Terminating the application...");
+        }
+
+        private static void OnTimedEvent(object source, ElapsedEventArgs e)
+        {
+            System.Drawing.Point mousePosition = Control.MousePosition;
+
+            Console.WriteLine("Time : {0} > MousePosition x : {1}, y : {2}", e.SignalTime, mousePosition.X, mousePosition.Y);
         }
     }
 }
